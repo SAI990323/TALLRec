04071a377b92aea2fa202cac2529a5c9d8d28cc2
baokq <sai990323@gmail.com>
2023-05-01 08:33:39 +0000

    update

--- shell/evaluate.sh
+++ shell/evaluate.sh
@@ -1,12 +1,15 @@
+# $2 means the type of the model (e.g instruct_movie)
 CUDA_ID=$1
 cp temp.json $2_book.json
-model_path=$(ls -d /home/sds/baokq/LLAMA4Rec/$2*)
+model_path=$(ls -d /model_path/$2*)
+base_model=XXX
+test_data=XXX
 for path in $model_path
 do
     echo $path
     CUDA_VISIBLE_DEVICES=$CUDA_ID python evaluate.py \
-        --base_model '/home/sds/baokq/LLAMA/hugging_face_LLAMA_weights_7B/' \
+        --base_model $base_model \
         --lora_weights $path \
-        --test_data_path '/home/sds/baokq/books/test.json' \
+        --test_data_path $test_data \
         --result_json_data $2_book.json
 done
